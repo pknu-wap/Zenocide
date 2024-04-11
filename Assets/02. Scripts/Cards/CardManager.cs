db89850db2bd94e61f90dc7964fec2af70d8bf3c
김동건 <125544913+ThisIsTAEWON@users.noreply.github.com>
2024-04-11 19:42:34 +0900

    CardOrder, DeckToHand, CardAlignment 구현
    CardOrder 마저 구현,
    덱에서 카드를 인스턴스화 헤서 핸드에 추가하는 드로우 구현,
    핸드에서 카드 정렬 구현

--- Assets/02. Scripts/Cards/CardManager.cs
+++ Assets/02. Scripts/Cards/CardManager.cs
@@ -11,28 +11,31 @@ public class CardManager : MonoBehaviour
 
     [SerializeField] ItemSO itemSO;
     [SerializeField] GameObject cardPrefab;
-    [SerializeField] List<CardData> hand; 
+    [SerializeField] List<Card> hand;
+    [SerializeField] Transform handLeft;
+    [SerializeField] Transform handRight;
+    [SerializeField] Transform cardSpawnPoint;
 
-    List<CardData> deck;
+    List<Item> deck;
 
-    public CardData DrawCard()
+    public Item DrawCard()
     {
         if (deck.Count == 0)
             SetUpDeck();
 
-        CardData card = deck[0];
+        Item card = deck[0];
         deck.RemoveAt(0);
         return card;
     }
 
     void SetUpDeck()
     {
-        deck = new List<CardData>(30);
+        deck = new List<Item>(30);
 
         // itemSO의 카드들을 deck에 추가
         for (int i = 0; i < itemSO.items.Length; i++) 
         {
-            CardData card = itemSO.items[i];
+            Item card = itemSO.items[i];
             deck.Add(card);
         }
 
@@ -40,7 +43,7 @@ public class CardManager : MonoBehaviour
         for (int i = 0; i < deck.Count; i++)
         {
             int rand = Random.Range(i, deck.Count);
-            CardData temp = deck[i];
+            Item temp = deck[i];
             deck[i] = deck[rand];
             deck[rand] = temp;
         }
@@ -54,17 +57,75 @@ public class CardManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Keypad1))
-            Debug.Log(DrawCard());
+            AddCardToHand();
     }
 
     void AddCardToHand()
     {
-        var cardObject = Instantiate(cardPrefab, Vector3.zero, Quaternion.identity);
-        var card = cardObject.GetComponent<CardInfoUpdater>();
-        card.UpdateCardInfo(DrawCard());
-        hand.Add(card.cardData);
+        var cardObject = Instantiate(cardPrefab, cardSpawnPoint.position, Utils.QI);
+        var card = cardObject.GetComponent<Card>();
+        card.Setup(DrawCard());
+        hand.Add(card);
 
-        //SetOriginOrder();
-        //CardAlignment(isMine);
+        SetOriginOrder();
+        CardAlignment();
+    }
+
+    void SetOriginOrder()
+    {
+        int count = hand.Count;
+        for (int i = 0; i < count; i++)
+        {
+            var targetCard = hand[i];
+            targetCard?.GetComponent<CardOrder>().SetOriginOrder(i);
+        }
+    }
+
+    void CardAlignment()
+    {
+        List<PRS> originCardPRSs = new List<PRS>();
+        originCardPRSs = RoundAlignment(handLeft, handRight, hand.Count, 0.5f, Vector3.one * 1.9f);
+
+        for (int i = 0; i < hand.Count; i++)
+        {
+            var targetCard = hand[i];
+
+            targetCard.originPRS = originCardPRSs[i];
+            targetCard.MoveTransform(targetCard.originPRS, true, 0.7f);
+        }
+    }
+
+    // 이해하기를 포기함...
+    List<PRS> RoundAlignment(Transform leftTr, Transform rightTr, int objCount, float height, Vector3 scale)
+    {
+        float[] objLerps = new float[objCount];
+        List<PRS> results = new List<PRS>(objCount);
+
+        switch (objCount)
+        {
+            case 1: objLerps = new float[] { 0.5f }; break;
+            case 2: objLerps = new float[] { 0.27f, 0.73f }; break;
+            case 3: objLerps = new float[] { 0.1f, 0.5f, 0.9f }; break;
+            default:
+                float interval = 1f / (objCount - 1);
+                for (int i = 0; i < objCount; i++)
+                    objLerps[i] = interval * i;
+                break;
+        }
+
+        for (int i = 0; i < objCount; i++)
+        {
+            var targetPos = Vector3.Lerp(leftTr.position, rightTr.position, objLerps[i]);
+            var targetRot = Utils.QI;
+            if (objCount >= 4)
+            {
+                float curve = Mathf.Sqrt(Mathf.Pow(height, 2) - Mathf.Pow(objLerps[i] - 0.5f, 2));
+                curve = height >= 0 ? curve : -curve;
+                targetPos.y += curve;
+                targetRot = Quaternion.Slerp(leftTr.rotation, rightTr.rotation, objLerps[i]);
+            }
+            results.Add(new PRS(targetPos, targetRot, scale));
+        }
+        return results;
     }
 }
--- Assets/02. Scripts/ScriptableObjects/ItemSO.cs
+++ Assets/02. Scripts/ScriptableObjects/ItemSO.cs
@@ -3,8 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Objects/Item SO")]
+[System.Serializable]
+public class Item
+{
+    public Sprite sprite;
+    public string name;
+    public int cost;
+    public string description;
+}
+
+[CreateAssetMenu(fileName = "ItemSO", menuName = "Scriptable Object/ItemSO")]
 public class ItemSO : ScriptableObject
 {
-    public CardData[] items;
+    public Item[] items;
 }
