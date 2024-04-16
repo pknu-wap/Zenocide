023805d43bf5e56405d4f2390a89d33d84c0b74f
차동근 <kkumteulyi@icloud.com>
2024-04-17 00:03:14 +0900

    24.04.17 텍스트 출력 수정
    텍스트 출력 수정
    - 기존 StoryScript에서 ','로 이름과 텍스트를 구분하던 방식에서 '#'으로 구분하도록 변경
    - 텍스트의 길이에 따라 사용자의 입력을 대기하는 표시인 WairCursor의 위치를 텍스트 끝에 오도록 수정

--- Assets/01. Scenes/CDG/Codes(UI)/DialogueManager.cs
+++ Assets/01. Scenes/CDG/Codes(UI)/DialogueManager.cs
@@ -6,24 +6,29 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    public TMP_Text dialogueText;
-    public TMP_Text dialogueName;
-    public GameObject dialogueBox;
-    public GameObject WaitCursor;
-    public Image dialogueImage;
-    public Sprite[] dialogueImages;
-    public string[] StoryText;
-    public string[] StoryName;
-    public int currentLine;
-
-    private bool isTyping = false;
-    private bool cancelTyping = false;
+    public TMP_Text dialogueText;        //Story Text
+    public TMP_Text dialogueName;        //Story Name
+      
+    public GameObject dialogueBox;       //전체 Canvas
+    public GameObject WaitCursor;        //다음 Text 대기 표시 커서
+      
+    public Image dialogueImage;          //일러스트
+          
+    public Sprite[] dialogueImages;      //일러스트 목록
+      
+    public string[] StoryText;           //Story Text 배열
+    public string[] StoryName;           //Story Name 배열
+      
+    public int currentLine;              //현재 출력 중인 문자열 위치
+      
+    private bool isTyping = false;       //타이핑 효과 진행 여부 확인 변수
+    private bool cancelTyping = false;   //
 
     void Start()
     {
-        dialogueBox.SetActive(false);
-        LoadDialogue();
-        ShowDialogue();
+        dialogueBox.SetActive(false);    //시작 시 Canvas 전체 비활성화
+        LoadDialogue();                  //Story Name,Text 불러오기
+        ShowDialogue();                  //이미지와 전체 Canvas 표시
     }
 
     void Update()
@@ -57,29 +62,41 @@ public class DialogueManager : MonoBehaviour
         }
     }
 
+    //Story Text에 타이핑 효과 추가하는 함수
     IEnumerator TypeSentence(string sentence)
     {
         isTyping = true;
         WaitCursor.SetActive(false);
         cancelTyping = false;
         dialogueText.text = "";
-
+        int posX = 0;
+        int posY = 0;
         foreach (char letter in sentence.ToCharArray())
         {
+            posX += 1;
             dialogueText.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.03f);
             if (cancelTyping)
             {
                 dialogueText.text = sentence;
                 break;
             }
         }
-
         isTyping = false;
+        if(posX >= 110){
+            posY = posX / 110;
+            posX %= 110;
+            posX += 2;
+        }
+        float textWidth = 33 * posX;
+        Debug.Log(textWidth);
+        Vector3 newPosition = dialogueText.transform.position + new Vector3(textWidth,-(5 + 60 * posY),0);
+        WaitCursor.transform.position = newPosition;
         WaitCursor.SetActive(true);
         cancelTyping = false;
     }
 
+    //초기 화면 표시
     public void ShowDialogue()
     {
         currentLine = -1;
@@ -88,8 +105,10 @@ public class DialogueManager : MonoBehaviour
         dialogueImage.sprite = dialogueImages[0];
     }
 
+    //TXT 파일에서 Story Text, Name 불러오는 함수
     void LoadDialogue()
     {
+        //파일 저장 경로
         string FilePath = "Assets/Resources/StoryScript.txt";
 
         StreamReader reader = new StreamReader(FilePath);
@@ -102,7 +121,7 @@ public class DialogueManager : MonoBehaviour
 
         for (int i = 0; i < StoryText.Length; i++)
         {
-            string[] Temp = StoryText[i].Split(',');
+            string[] Temp = StoryText[i].Split('#');
             StoryText[i] = Temp[1]; // 대화 문장 저장
             StoryName[i] = Temp[0]; // 대화 이름 저장
         }
