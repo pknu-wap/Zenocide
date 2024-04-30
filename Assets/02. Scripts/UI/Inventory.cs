5863c72986151f232cd8c96a1e11f3d424b0ee47
WooMH <woominho4756@gmail.com>
2024-04-30 21:30:31 +0900

    인벤토리, 아이템 추가
    1. 화면 우측상단에 인벤토리 온오프 버튼 제작
    2. Slots에 Items에 있는 아이템 버튼 누를 시 랜덤으로 추가
    3. 랜덤으로 추가된 아이템은 Items 리스트에서 삭제

--- Assets/02. Scripts/UI/Inventory.cs
+++ Assets/02. Scripts/UI/Inventory.cs
@@ -3,44 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class Inventory : MonoBehaviour
 {
-    public List<string> items = new List<string>();
-    // TMP_Text slot;
-    public List<TMP_Text> slots = new List<TMP_Text>();
+    public GameObject invnetoryPanel;
+    bool toggleInvenyory = false;
     void Start()
     {
-        items.Add("배고픔");
-        items.Add("무기");
-        items.Add("근력");
-        items.Add("속도");
-        items.Add("빵");
-        items.Add("라면");
-        // slot = transform.GetChild(1).GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>();
-        slots = transform.GetChild(1).GetChild(0).GetChild(0).GetComponentsInChildren<TMP_Text>().ToList();
+        invnetoryPanel.SetActive(toggleInvenyory);
     }
 
     void Update()
     {
         
     }
-    public void AddItem(string item)
+    public void ToggleInventory()
     {
-        items.Add(item);
-    }
-    public void ShowItems()
-    {
-        // TMP_Texts.text = items[0];
-        int i = 0;
-        for (; i < items.Count; i++)
-        {
-            slots[i].text = items[i];
-            slots[i].gameObject.SetActive(true);
-        }
-        for (; i < slots.Count; i++)
-        {
-            slots[i].gameObject.SetActive(false);
-        }
+        toggleInvenyory = !toggleInvenyory;
+        invnetoryPanel.SetActive(toggleInvenyory);
     }
 }
