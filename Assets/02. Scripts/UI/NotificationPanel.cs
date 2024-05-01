131de386613b37f971a9980094dc75520db549bb
miniron_v <61517039+miniron-v@users.noreply.github.com>
2024-05-02 01:15:01 +0900

    Notification 연출 변경
    스케일이 (0, 0, 0)에서 (1, 1, 1)로 변하던 걸 y값만 0에서 1로 변하게 수정했다.

--- Assets/02. Scripts/UI/NotificationPanel.cs
+++ Assets/02. Scripts/UI/NotificationPanel.cs
@@ -14,7 +14,7 @@ public class NotificationPanel : MonoBehaviour
         Sequence sequence = DOTween.Sequence()
             .Append(transform.DOScale(Vector3.one, 0.3f).SetEase(Ease.InOutQuad))
             .AppendInterval(0.9f)
-            .Append(transform.DOScale(Vector3.zero, 0.3f).SetEase(Ease.InOutQuad));
+            .Append(transform.DOScale(new Vector3(1, 0, 1), 0.3f).SetEase(Ease.InOutQuad));
     }
 
     void Start() => ScaleZero();
@@ -23,5 +23,5 @@ public class NotificationPanel : MonoBehaviour
     void SclaeOne() => transform.localScale = Vector3.one;
 
     [ContextMenu("ScaleZero")]
-    public void ScaleZero() => transform.localScale = Vector3.zero;
+    public void ScaleZero() => transform.localScale = new Vector3(1, 0, 1);
 }
