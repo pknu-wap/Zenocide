d96194afdeaa179809101d62f4e82b9b04f27bf6
miniron_v <61517039+miniron-v@users.noreply.github.com>
2024-06-02 01:25:13 +0900

    타이핑 출력 후, 선택지 띄우기만 하기
    선택 기능은 아직 버그가 있다.

--- Assets/02. Scripts/Tutorial/DiaryManager.cs
+++ Assets/02. Scripts/Tutorial/DiaryManager.cs
@@ -1,20 +1,50 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DiaryManager : MonoBehaviour
 {
+    public static DiaryManager Instance { get; set; }
+
+    [Header("스크립트 에셋")]
     // CSV 내용이 담겨 있는 딕셔너리 리스트
     private List<Dictionary<string, object>> diaryDialog;
-    public TextAsset diaryCSV;
+    [SerializeField] private TextAsset diaryCSV;
 
+    [Header("컴포넌트")]
+    [SerializeField] private PageScripter scripter;
+    [SerializeField] private Transform choiceParent;
+    [SerializeField] private TMP_Text[] choiceButtons;
+
+    [Header("현재 다이얼로그 정보")]
     // 현재 출력 중인 텍스트 번호
-    private int dialogIndex;
+    [SerializeField] private int dialogIndex = 0;
     // 현재 출력 중인 페이지 번호
-    private int pageIndex;
+    private int pageIndex = 0;
+    // 선택된 단어
+    [SerializeField] private string selectedWord;
 
     private void Awake()
     {
+        // 싱글톤
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        choiceButtons = new TMP_Text[choiceParent.childCount];
+        for(int i = 0; i < choiceParent.childCount; ++i)
+        {
+            choiceButtons[i] = choiceParent.GetChild(i).GetChild(0).GetComponent<TMP_Text>();
+        }
+        choiceParent.gameObject.SetActive(false);
+
         diaryDialog = CSVReader.Read(diaryCSV);
 
         Debug.Log(diaryDialog[0]["내용"]);
@@ -23,6 +53,44 @@ public class DiaryManager : MonoBehaviour
     // 문장을 넘겨준다.
     public void AddDialog()
     {
+        // 출력에 성공한다면
+        if (scripter.ShowDialog(diaryDialog[dialogIndex]) == true)
+        {
+            // 다음 줄로 이동한다.
+            ++dialogIndex;
+        }
+    }
 
+    public bool isSelected = false;
+
+    public IEnumerator ShowChoiceButton()
+    {
+        // 선택지를 띄운다.
+        for(int i = 0; i < choiceButtons.Length; ++i)
+        {
+            choiceButtons[i].text = diaryDialog[dialogIndex][i.ToString()].ToString();
+        }
+        choiceParent.gameObject.SetActive(true);
+
+        // 선택할 때까지 기다린다.
+        while (isSelected == false)
+        {
+            yield return null;
+        }
+
+        // scripter에 전달해준다.
+        scripter.Select(selectedWord);
+
+        // 다시 false로 돌리고 종료
+        isSelected = false;
+    }
+
+    // 선택한다.
+    public void Select(int i)
+    {
+        selectedWord = diaryDialog[dialogIndex][i.ToString()].ToString();
+
+        choiceParent.gameObject.SetActive(false);
+        isSelected = true;
     }
 }
--- Assets/02. Scripts/Tutorial/PageScripter.cs
+++ Assets/02. Scripts/Tutorial/PageScripter.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PageScripter : MonoBehaviour
@@ -13,12 +12,36 @@ public class PageScripter : MonoBehaviour
     // 현재 타이핑 중인가?
     private bool isTyping = false;
     private bool cancelTyping = false;
-    private float typingTime = 0.03f;
+    // 현재까지 적힌 텍스트
     private string currentDialog = "";
+    // 현재 진행 중인 행
+    private Dictionary<string, object> currentLine;
+    // 선택된 단어
+    [SerializeField] private string selectedWord;
 
-    public void ShowDialog(string sentence)
+    [Header("타이핑 시간")]
+    private const float typingTime = 0.03f;
+    private const float meetMarkTime = 0.5f;
+
+    private void Awake()
     {
+        diaryText = transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>();
+    }
+
+    public bool ShowDialog(Dictionary<string, object> line)
+    {
+        if (isTyping)
+        {
+            // 실패했음을 알린다.
+            return false;
+        }
+
+        currentLine = line;
+        string sentence = line["내용"].ToString();
+
         StartCoroutine(TypeSentence(sentence));
+        // 성공했음을 알린다.
+        return true;
     }
 
     private IEnumerator TypeSentence(string sentence)
@@ -31,23 +54,57 @@ public class PageScripter : MonoBehaviour
         cancelTyping = false;
         diaryText.text = currentDialog;
 
-        foreach (char letter in sentence)
+        for (int i = 0; i < sentence.Length; ++i)
         {
+            // 한 글자만 떼온다.
+            char letter = sentence[i];
+
+            // 해당 글자가 %라면
+            if(letter == '%')
+            {
+                // 선택지를 띄우고, 응답을 기다린다.
+                yield return StartCoroutine(DiaryManager.Instance.ShowChoiceButton());
+
+                letter = '\0';
+                diaryText.text += selectedWord;
+            }
+
+            // letter를 추가하고
             diaryText.text += letter;
 
-            yield return new WaitForSeconds(typingTime);
+            // delay를 준다.
+            float delay = typingTime;
+            // 문장 부호로 끝난다면 더 길게 준다.
+            if (letter == ',' || letter == '.')
+            {
+                delay = meetMarkTime;
+            }
+
+            yield return new WaitForSeconds(delay);
 
             // 타이핑 효과 취소 시 대화를 한번에 출력
             if (cancelTyping)
             {
-                diaryText.text = currentDialog + sentence;
+                diaryText.text = currentDialog + sentence + "\n";
                 break;
             }
         }
+
         // 대화 진행 종료
         isTyping = false;
         // waitCursor.SetActive(true);
         // 현재 텍스트 갱신
-        currentDialog += sentence;
+        currentDialog += sentence + "\n";
+    }
+
+    public void Select(string word)
+    {
+        selectedWord = word;
+    }
+
+    // 타이핑 효과를 취소한다.
+    public void CancelTyping()
+    {
+        cancelTyping = false;
     }
 }
