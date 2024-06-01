0b0ab72966b2a3de1ac0d2edb2e5be3d14019f80
miniron_v <61517039+miniron-v@users.noreply.github.com>
2024-06-01 21:54:04 +0900

    CSV 테스트 전 커밋

--- Assets/02. Scripts/Tutorial/DiaryManager.cs
+++ Assets/02. Scripts/Tutorial/DiaryManager.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class DiaryManager : MonoBehaviour
 {
+    private List<Dictionary<string, object>> diaryDialog;
 
+    private void Awake()
+    {
+        diaryDialog = CSVReader.Read("Assets/NoShare2024-1/NoShare2024-1/04. Scenarios/Tutorial Diary");
+
+        Debug.Log(diaryDialog[0]["내용"]);
+    }
 }
