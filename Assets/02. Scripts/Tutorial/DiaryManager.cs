553ca4244377ba2fcccc80c0839841b77f9514c9
miniron_v <61517039+miniron-v@users.noreply.github.com>
2024-06-01 22:24:27 +0900

    CSV 불러오기
    경로를 사용하지 못 하니 CSVReader의 Read 함수를 다형화했고, 이를 Debug로 테스트했다. 결과는 성공
    
    인스펙터에 직접 CSV 파일을 끌어다 놔야 한다.

--- Assets/02. Scripts/Tutorial/DiaryManager.cs
+++ Assets/02. Scripts/Tutorial/DiaryManager.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class DiaryManager : MonoBehaviour
 {
+    // CSV 내용이 담겨 있는 딕셔너리 리스트
     private List<Dictionary<string, object>> diaryDialog;
+    public TextAsset diaryCSV;
+
+    // 현재 출력 중인 텍스트 번호
+    private int dialogIndex;
+    // 현재 출력 중인 페이지 번호
+    private int pageIndex;
 
     private void Awake()
     {
-        diaryDialog = CSVReader.Read("Tutorial Diary");
+        diaryDialog = CSVReader.Read(diaryCSV);
 
         Debug.Log(diaryDialog[0]["내용"]);
+    }
+
+    // 문장을 넘겨준다.
+    public void AddDialog()
+    {
+
     }
 }
--- Assets/02. Scripts/Tutorial/PageScripter.cs
+++ Assets/02. Scripts/Tutorial/PageScripter.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class PageScripter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("컴포넌트")]
+    public TMP_Text diaryText;
+
+    [Header("상태 체크")]
+    // 현재 타이핑 중인가?
+    private bool isTyping = false;
+    private bool cancelTyping = false;
+    private float typingTime = 0.03f;
+    private string currentDialog = "";
+
+    public void ShowDialog(string sentence)
     {
-        
+        StartCoroutine(TypeSentence(sentence));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator TypeSentence(string sentence)
     {
-        
+        // 대화 진행 시작
+        isTyping = true;
+        // 다음 대화로 넘어가기 전에 기다리는 커서 비활성화
+        // waitCursor.SetActive(false);
+
+        cancelTyping = false;
+        diaryText.text = currentDialog;
+
+        foreach (char letter in sentence)
+        {
+            diaryText.text += letter;
+
+            yield return new WaitForSeconds(typingTime);
+
+            // 타이핑 효과 취소 시 대화를 한번에 출력
+            if (cancelTyping)
+            {
+                diaryText.text = currentDialog + sentence;
+                break;
+            }
+        }
+        // 대화 진행 종료
+        isTyping = false;
+        // waitCursor.SetActive(true);
+        // 현재 텍스트 갱신
+        currentDialog += sentence;
     }
 }
