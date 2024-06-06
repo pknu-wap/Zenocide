8ed8722a967f1507ea4e271c62400cf45cc44786
taewon <donggunmaru@gmail.com>
2024-06-06 13:46:56 +0900

    타겟록 스크립트 스테이터스오프너 스크립트에 병합
    레이 캐스트 인식 문제로 같은 스크립트로 병합한다.

--- Assets/02. Scripts/Battle/UI/StatePanelOpener.cs
+++ Assets/02. Scripts/Battle/UI/StatePanelOpener.cs
@@ -6,19 +6,40 @@ using UnityEngine.UI;
 
 public class StatePanelOpener : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    // 스테이터스 패널
     [SerializeField] GameObject statePanel;
-    // [SerializeField] Image statePanelImage;
+
+    // 타겟록
+    GameObject targetLock;
+    Image illust;
+
+    // 캔버스
     [SerializeField] protected Canvas uiCanvas;
+
+    // 마우스 이벤트 블로커
     public GameObject mouseEventBlocker;
+
+    // [SerializeField] Image statePanelImage;
 
     private void Awake()
     {
+        uiCanvas = transform.parent.GetComponent<Canvas>();
+
+        // 스테이터스 패널
         statePanel = transform.parent.GetChild(0).gameObject;
         // statePanelImage = statePanel.GetComponent<Image>();
-        uiCanvas = transform.parent.GetComponent<Canvas>();
-        mouseEventBlocker = GameObject.Find("Mouse Event Blocker");
 
         statePanel.SetActive(false);
+
+        uiCanvas = transform.parent.GetComponent<Canvas>();
+        targetLock = transform.parent.parent.GetChild(0).GetChild(2).gameObject;
+        illust = transform.parent.parent.GetChild(0).GetChild(0).GetComponent<Image>();
+        float width = illust.rectTransform.sizeDelta.x;
+        float height = illust.rectTransform.sizeDelta.y;
+
+        targetLock.SetActive(false);
+
+        mouseEventBlocker = GameObject.Find("Mouse Event Blocker");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -39,6 +60,13 @@ public class StatePanelOpener : MonoBehaviour, IPointerEnterHandler, IPointerExi
             // 패널을 활성화 시킨다.
             statePanel.SetActive(true);
         }
+
+        // 타겟록
+        if (CardArrow.Instance.gameObject.activeSelf)
+        {
+            uiCanvas.sortingOrder = 10;
+            targetLock.SetActive(true);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -55,5 +83,8 @@ public class StatePanelOpener : MonoBehaviour, IPointerEnterHandler, IPointerExi
         uiCanvas.sortingOrder = 0;
         // 패널을 비활성화 시킨다.
         statePanel.SetActive(false);
+
+        // 타겟록
+        targetLock.SetActive(false);
     }
 }
