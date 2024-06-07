53fc737524031345723b4770476a7fb199160272
miniron_v <61517039+miniron-v@users.noreply.github.com>
2024-06-07 14:52:00 +0900

    씬 이동 버튼이 여러 번 눌리는 현상 해결

--- Assets/02. Scripts/Lobby/SceneMoveButton.cs
+++ Assets/02. Scripts/Lobby/SceneMoveButton.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class SceneMoveButton : MonoBehaviour
 {
+    private bool isMoved = false;
+
     public void MoveScene(string sceneName)
     {
+        // 연속으로 눌러지는 걸 막는다.
+        if (isMoved)
+        {
+            return;
+        }
+
         SceneLoader.Instance.LoadScene(sceneName);
+        isMoved = true;
     }
 }
