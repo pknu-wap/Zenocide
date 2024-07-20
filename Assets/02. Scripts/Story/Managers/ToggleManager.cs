672b9b4d141e2aeee31db9ea864d83fa5e1b490c
WooMH <woominho4756@gmail.com>
2024-07-20 23:45:08 +0900

    UI OnOff 수정
    인벤 켜진 상태에서 덱 키면 인벤은 꺼지고 덱만 켜지게 수정(다른 UI에도 모두 적용)

---
+++ Assets/02. Scripts/Story/Managers/ToggleManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    public GameObject deckPanel;
+    public GameObject optionPanel;
+
+    private bool activeInventory = false;
+    private bool activeDeck = false;
+    private bool activeOption = false;
+
+    void Start()
+    {
+        inventoryPanel.SetActive(activeInventory);
+        deckPanel.SetActive(activeDeck);
+        optionPanel.SetActive(activeOption);
+    }
+
+    void Update()
+    {
+        // Panel이 켜진 상태에서 ESC 누르면 꺼짐
+        if (activeInventory == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            activeInventory = !activeInventory;
+            inventoryPanel.SetActive(activeInventory);
+        }
+        if (activeDeck == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            activeDeck = !activeDeck;
+            deckPanel.SetActive(activeDeck);
+        }
+        if (activeOption == true && Input.GetKeyDown(KeyCode.Escape))
+        {
+            activeOption = !activeOption;
+            optionPanel.SetActive(activeOption);
+        }
+    }
+    // aPanel이 켜진 상태에서 bPanel을 키면 aPanel 꺼지고 bPanel 켜짐
+    public void ToggleInventory()
+    {
+        if (!activeInventory)
+        {
+            if (activeDeck)
+            {
+                ToggleDeck(false);
+            }
+            if (activeOption)
+            {
+                ToggleOption(false);
+            }
+        }
+        activeInventory = !activeInventory;
+        inventoryPanel.SetActive(activeInventory);
+    }
+    public void ToggleDeck()
+    {
+        if (!activeDeck)
+        {
+            if (activeInventory)
+            {
+                ToggleInventory(false);
+            }
+            if (activeOption)
+            {
+                ToggleOption(false); 
+            }
+        }
+        activeDeck = !activeDeck;
+        deckPanel.SetActive(activeDeck);
+    }
+    public void ToggleOption()
+    {
+        if (!activeOption)
+        {
+            if (activeInventory)
+            {
+                ToggleInventory(false); 
+            }
+            if (activeDeck)
+            {
+                ToggleDeck(false);
+            }
+        }
+        activeOption = !activeOption;
+        optionPanel.SetActive(activeOption);
+    }
+    // Panel이 true면 활성화, false면 비활성화
+    public void ToggleInventory(bool state)
+    {
+        activeInventory = state;
+        inventoryPanel.SetActive(activeInventory);
+    }
+
+    public void ToggleDeck(bool state)
+    {
+        activeDeck = state;
+        deckPanel.SetActive(activeDeck);
+    }
+
+    public void ToggleOption(bool state)
+    {
+        activeOption = state;
+        optionPanel.SetActive(activeOption);
+    }
+}
--- Assets/02. Scripts/Story/StoryUI/Inventory.cs
+++ Assets/02. Scripts/Story/StoryUI/Inventory.cs
@@ -10,23 +10,6 @@ public class Inventory : MonoBehaviour
     public List<TMP_Text> slots = new List<TMP_Text>();
     private void Start()
     {
-        // 게임 시작 시 인벤토리 안 보이게
-        inventoryPanel.SetActive(activeInventory);
         slots = transform.GetChild(0).GetChild(0).GetChild(0).GetComponentsInChildren<TMP_Text>().ToList();
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            activeInventory = !activeInventory;
-            inventoryPanel.SetActive(activeInventory);
-        }
-    }
-    public void ToggleInventory()
-    {
-        // Pointer Enter, Exit시 false -> true, true -> false로 바꾼다
-        activeInventory = !activeInventory;
-        // activeInventory가 false인지 true인지에 따라 Panel이 보일지 말지 결정한다.
-        inventoryPanel.SetActive(activeInventory); 
     }
 }
