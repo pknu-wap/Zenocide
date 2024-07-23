6f9650e263a7267a6cea5fa46962b939fecd30ca
WooMH <woominho4756@gmail.com>
2024-07-24 04:33:53 +0900

    해상도 수정
    해상도 관련 수정

--- Assets/02. Scripts/Story/StoryUI/VideoOption.cs
+++ Assets/02. Scripts/Story/StoryUI/VideoOption.cs
@@ -4,46 +4,64 @@ using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using TMPro;
 
 public class VideoOption : MonoBehaviour
 {
-    public Dropdown resolutionDropdown;
+    public TMP_Dropdown resolutionDropdown;
     public Toggle fullscreenToggle;
+    public Button applyButton;
+
     private Resolution[] resolutions;
-    private List<string> options = new List<string>();
+    private bool fullscreen;
+    private int selectedResolutionIndex;
     void Start()
     {
-        // Dropdown 초기화
+        // 해상도 리스트 가져오기
         resolutions = Screen.resolutions;
-        // 현재 해상도 Index 초기화
-        int currentResolutionIndex = 0;
-        // 모든 해상도 순회하며 Dropdown에 options 추가
+        resolutionDropdown.ClearOptions();
+
+        // Dropdown에 옵션 추가하기
+        List<string> options = new List<string>();
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+        }
+        resolutionDropdown.AddOptions(options);
 
+        // 현재 해상도 선택
+        selectedResolutionIndex = FindCurrentResolutionIndex();
+        resolutionDropdown.value = selectedResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        // 토글 상태 설정
+        fullscreenToggle.isOn = Screen.fullScreen;
+
+        // 버튼 클릭 시 적용
+        applyButton.onClick.AddListener(ApplySettings);
+    }
+    // 현재 해상도의 인덱스를 찾는 메서드
+    private int FindCurrentResolutionIndex()
+    {
+        for (int i = 0; i < resolutions.Length; i++)
+        {
             if (resolutions[i].width == Screen.currentResolution.width &&
                 resolutions[i].height == Screen.currentResolution.height)
             {
-                currentResolutionIndex = i;
+                return i;
             }
         }
-        // Dropdown에 옵션들을 추가하고 현재 해상도를 선택함
-        resolutionDropdown.AddOptions(options); 
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-        // 전체화면 설정을 Toggle에 반영
-        fullscreenToggle.isOn = Screen.fullScreen;
+        return 0;
     }
-    // 설정 저장 함수
-    public void SaveSettings()
+    // 설정 적용 메서드
+    public void ApplySettings()
     {
-        // 해상도 설정
-        int resolutionIndex = resolutionDropdown.value;
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, fullscreenToggle.isOn);
-        // 전체화면 설정
-        Screen.fullScreen = fullscreenToggle.isOn;
+        selectedResolutionIndex = resolutionDropdown.value;
+        Resolution resolution = resolutions[selectedResolutionIndex];
+        fullscreen = fullscreenToggle.isOn;
+
+        // 해상도와 전체화면 설정 적용
+        Screen.SetResolution(resolution.width, resolution.height, fullscreen);
     }
 }
