098c13ec169970363e8a7495616802540bc3e8f4
ThisIsTAEWON <125544913+ThisIsTAEWON@users.noreply.github.com>
2024-07-31 19:23:28 +0900

    Data 및 DataManager 추가
    Data 객체와 객체의 json 파일로의 save, load 구현,
    json 파일 암호화 구현

---
+++ Assets/02. Scripts/etc/Data.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Data
+{
+    public int hp;
+    public List<CardData> deck;
+    // 직업
+    // 처리 가능 이벤트 리스트
+    // 딜레이 딕셔너리
+    // 아이템 인벤토리
+}
