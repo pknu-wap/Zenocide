d1d983159a6b10148591539cfe148d90c92278aa
ThisIsTAEWON <125544913+ThisIsTAEWON@users.noreply.github.com>
2024-08-01 15:29:42 +0900

    데이터 객체 갱신

--- Assets/02. Scripts/etc/Data.cs
+++ Assets/02. Scripts/etc/Data.cs
@@ -5,10 +5,91 @@ using UnityEngine;
 [Serializable]
 public class Data
 {
+    // 체력
     public int hp;
+    // 덱
     public List<CardData> deck;
     // 직업
-    // 처리 가능 이벤트 리스트
+    public string job;
+    // 현재 이벤트
+    public EventData currentEvent;
+    // 이벤트 리스트
+    public List<EventData> processableMainEventList;
+    public List<EventData> processableSubEventList;
     // 딜레이 딕셔너리
+    public Dictionary<EventData, int> delayDictionary;
     // 아이템 인벤토리
+    public Dictionary<string, int> items;
+    //옵션
+    public int selectedResolutionIndex;
+    public bool fullscreen;
+    public float masterVolume;
+    public float bgmVolume;
+
+
+    public int Hp
+    {
+        get { return hp; }
+        set { hp = value; }
+    }
+
+    public List<CardData> Deck
+    {
+        get { return deck; }
+        set { deck = value; }
+    }
+
+    public EventData CurrentEvent
+    {
+        get { return currentEvent; }
+        set { currentEvent = value; }
+    }
+
+    public List<EventData> ProcessableMainEventList
+    {
+        get { return processableMainEventList; }
+        set { processableMainEventList = value; }
+    }
+
+    public List<EventData> ProcessableSubEventList
+    {
+        get { return processableSubEventList; }
+        set { processableSubEventList = value; }
+    }
+
+    public Dictionary<EventData, int> DelayDictionary
+    {
+        get { return delayDictionary; }
+        set { delayDictionary = value; }
+    }
+
+    public Dictionary<string, int> Items
+    {
+        get { return items; }
+        set { items = value; }
+    }
+
+    public int SelectedResolutionIndex
+    {
+        get { return selectedResolutionIndex; }
+        set { selectedResolutionIndex = value; }
+    }
+
+    public bool Fullscreen
+    {
+        get { return fullscreen; }
+        set {  fullscreen = value; }
+    }
+
+    public float MasterVolume
+    {
+        get { return masterVolume; }
+        set {  masterVolume = value; }
+    }
+
+    public float BgmVolume
+    {
+        get { return bgmVolume; }
+        set { bgmVolume = value; }
+    }
 }
