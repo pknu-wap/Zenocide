93e303b6ff685355d46fdb5e59dde6a6182f9ef2
차동근 <127104912+Kkumteulyi@users.noreply.github.com>
2024-09-09 23:22:59 +0900

    화면 흔들기 수정
    튜토리얼씬, 스토리씬, 배틀씬 모두 적용가능하도록 구조를 변경하였습니다

--- Assets/02. Scripts/etc/CamShake.cs
+++ Assets/02. Scripts/etc/CamShake.cs
@@ -5,12 +5,16 @@ public class CamShake : MonoBehaviour
 {
     public static CamShake Instance { get; set; }
 
-    public Canvas canvas;         // 카메라가 비추는 캔버스
+    public Camera tutorialCam;    // 튜토리얼씬 카메라
+    public Canvas tutorialCanvas; // 튜토리얼씬 캔버스
+    public Camera storyCam;       // 스토리씬 카메라
+    public Canvas storyCanvas;    // 스토리씬 캔버스
+    public Camera battleCam;      // 전투씬 카메라
+    public Canvas battleCanvas;   // 전투씬 캔버스
     private Vector3 originalPos;  // 카메라 원래 위치
-
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
         }
@@ -19,24 +23,52 @@ public class CamShake : MonoBehaviour
             Destroy(this);
             return;
         }
-        originalPos = transform.localPosition; // 초기 위치를 저장
     }
 
-    public void Shake(float duration = 0.2f, float magnitude = 5f)
+    public void test()
     {
-        canvas.renderMode = RenderMode.WorldSpace;        // 캔버스 렌더링 모드를 월드로 변경(기본값은 스크린)
-        StartCoroutine(CoroutineShake(duration, magnitude));
+        Shake();
     }
 
-    public IEnumerator CoroutineShake(float duration, float magnitude)
+    //camNum = 0(스토리씬 카메라를 기본값으로 설정) 스토리씬: 0 / 전투씬: 1 / 튜토리얼: 2
+    public void Shake(float duration = 0.2f, float magnitude = 5f, int camNum = 0)
+    {
+        Camera currentCam;
+        Canvas currentCanvas;
+        switch(camNum)
+        {
+            case 0: // 스토리씬 카메라
+                currentCam = storyCam;
+                currentCanvas = storyCanvas;
+                break;
+            case 1: // 배틀씬 카메라
+                currentCam = battleCam;
+                currentCanvas = battleCanvas;
+                break;
+            case 2: // 튜토리얼씬 카메라
+                currentCam = tutorialCam;
+                currentCanvas = tutorialCanvas;
+                break;
+            default: // 기본값 = 스토리씬 카메라
+                currentCam = storyCam;
+                currentCanvas = battleCanvas;
+                break;
+        }
+
+        currentCanvas.renderMode = RenderMode.WorldSpace;    // 캔버스 렌더링 모드를 월드로 변경(기본값은 스크린)
+        originalPos = currentCam.transform.localPosition;    // 초기 위치를 저장
+        StartCoroutine(CoroutineShake(duration, magnitude, currentCam, currentCanvas));
+    }
+
+    public IEnumerator CoroutineShake(float duration, float magnitude,Camera cam, Canvas canvas)
     {
         while(duration > 0)
         {
-            transform.localPosition = originalPos + Random.insideUnitSphere * magnitude; // 랜덤하게 흔들기
+            cam.transform.localPosition = originalPos + Random.insideUnitSphere * magnitude; // 무작위로 흔들기
             duration -= Time.deltaTime;
             yield return null;
         }
-        transform.localPosition = originalPos;            // 원래 위치로 복구
+        cam.transform.localPosition = originalPos;        // 원래 위치로 복구
         canvas.renderMode = RenderMode.ScreenSpaceCamera; // 캔버스 렌더링 모드를 기본값(스크린)으로 변경
     }
 
