e4b3bd7cdf8d1b2d9d827eefdc6af4df4b461889
차동근 <127104912+Kkumteulyi@users.noreply.github.com>
2024-09-10 01:50:16 +0900

    화면 흔들림 효과 수정
    1. 화면 흔들림 효과를 부여할 때 캔버스의 크기가 약간 확장된 뒤 효과가 나타나도록 수정
    2. int형 매개 변수를 사용하는 대신 enum을 사용하여 직관성 개선
    ex) scene.story , scene.battle , scene.tutorial

--- Assets/02. Scripts/etc/CamShake.cs
+++ Assets/02. Scripts/etc/CamShake.cs
@@ -7,11 +7,26 @@ public class CamShake : MonoBehaviour
 
     public Camera tutorialCam;    // 튜토리얼씬 카메라
     public Canvas tutorialCanvas; // 튜토리얼씬 캔버스
+    
     public Camera storyCam;       // 스토리씬 카메라
     public Canvas storyCanvas;    // 스토리씬 캔버스
+    
     public Camera battleCam;      // 전투씬 카메라
     public Canvas battleCanvas;   // 전투씬 캔버스
+    
     private Vector3 originalPos;  // 카메라 원래 위치
+    
+    private RectTransform storyCanvasRect;      // 스토리씬 캔버스 사이즈 정보
+    private RectTransform battleCanvasRect;     // 배틀씬 캔버스 사이즈 정보
+    private RectTransform tutorialCanvasRect;   // 튜토리얼씬 캔버스 사이즈 정보
+
+    public enum scene
+    {
+        story,
+        battle,
+        tutorial
+    };
+
     private void Awake()
     {
         if (Instance == null)
@@ -23,41 +38,55 @@ public class CamShake : MonoBehaviour
             Destroy(this);
             return;
         }
+        storyCanvasRect = storyCanvas.GetComponent<RectTransform>();       // 스토리씬 캔버스 정보 불러옴
+        battleCanvasRect = battleCanvas.GetComponent<RectTransform>();     // 배틀씬 캔버스 정보 불러옴
+        tutorialCanvasRect = tutorialCanvas.GetComponent<RectTransform>(); // 튜토리얼씬 캔버스 정보 불러옴
     }
 
     public void test()
     {
-        Shake();
+        Shake(0.2f,5f,scene.story);
     }
 
-    //camNum = 0(스토리씬 카메라를 기본값으로 설정) 스토리씬: 0 / 전투씬: 1 / 튜토리얼: 2
-    public void Shake(float duration = 0.2f, float magnitude = 5f, int camNum = 0)
+    //currentScene = scene.story(스토리씬 카메라를 기본값으로 설정) 스토리씬: scene.story / 전투씬: scene.battle / 튜토리얼: scene.tutorial
+    public void Shake(float duration = 0.2f, float magnitude = 5f, scene currentScene = scene.story)
     {
         Camera currentCam;
         Canvas currentCanvas;
-        switch(camNum)
+        RectTransform currentCanvasRect;
+        RectTransform originalCanvasRect;
+        switch(currentScene)
         {
-            case 0: // 스토리씬 카메라
+            case scene.story: // 스토리씬 카메라
                 currentCam = storyCam;
                 currentCanvas = storyCanvas;
+                currentCanvasRect = storyCanvasRect;
                 break;
-            case 1: // 배틀씬 카메라
+            case scene.battle: // 배틀씬 카메라
                 currentCam = battleCam;
                 currentCanvas = battleCanvas;
+                currentCanvasRect = battleCanvasRect;
                 break;
-            case 2: // 튜토리얼씬 카메라
+            case scene.tutorial: // 튜토리얼씬 카메라
                 currentCam = tutorialCam;
                 currentCanvas = tutorialCanvas;
+                currentCanvasRect = tutorialCanvasRect;
                 break;
             default: // 기본값 = 스토리씬 카메라
                 currentCam = storyCam;
-                currentCanvas = battleCanvas;
+                currentCanvas = storyCanvas;
+                currentCanvasRect = storyCanvasRect;
                 break;
         }
 
-        currentCanvas.renderMode = RenderMode.WorldSpace;    // 캔버스 렌더링 모드를 월드로 변경(기본값은 스크린)
-        originalPos = currentCam.transform.localPosition;    // 초기 위치를 저장
-        StartCoroutine(CoroutineShake(duration, magnitude, currentCam, currentCanvas));
+        currentCanvas.renderMode = RenderMode.WorldSpace;           // 캔버스 렌더링 모드를 월드로 변경(기본값은 스크린)
+        originalPos = currentCam.transform.localPosition;           // 기본 위치 저장
+        originalCanvasRect = currentCanvasRect;                     // 기본 캔버스 정보 저장
+        float currentCanvasHeight = currentCanvasRect.rect.height;  // 캔버스의 높이 정보 불러옴
+        float currentCanvasWidth = currentCanvasRect.rect.width;    // 캔버스의 너비 정보 불러옴
+        currentCanvasRect.sizeDelta = new Vector2(currentCanvasWidth + 200, currentCanvasHeight + 200); // 높이와 너비에 각각 200씩 추가하여 여백이 보이지 않도록 확장
+        StartCoroutine(CoroutineShake(duration, magnitude, currentCam, currentCanvas)); // 화면 흔들림 효과를 주는 코루틴 함수 실행
+        currentCanvasRect = originalCanvasRect; // 흔들기 함수 종료 후 캔버스 사이즈 복구
     }
 
     public IEnumerator CoroutineShake(float duration, float magnitude,Camera cam, Canvas canvas)
